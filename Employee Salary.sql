1dcd108b967817357fb9a0873bd51067174824c5
Md. Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-09-05 22:54:34 +0600

    Modify

--- Employee Salary.sql
+++ Employee Salary.sql
@@ -64,46 +64,50 @@ order by 1
 
 
 --- Terminated Employee Salary Balance
+
 SET ANSI_WARNINGS OFF
 DECLARE @StartDate date = '2021-08-01';
 DECLARE @EndDate date = '2021-09-01';
 DECLARE @RunTime INT;
 DECLARE @TerminatedEmployee TABLE (
-ID INT IDENTITY(1, 1) PRIMARY KEY
-,EmployeeID INT
-,TerminatedOn DATE
+        ID INT IDENTITY(1, 1) PRIMARY KEY
+        ,EmployeeID INT
+        ,TerminatedOn DATE
 );
 DECLARE @TerminatedEmployeeMoneyBalance TABLE (
-CustomerID INT
-,BadgeID INT
-,FullName NVARCHAR(64)
-,DesignationName NVARCHAR(64)
-,JoinedOn DATE
-,TerminatedOn DATE
-,AccountThead NVARCHAR(64)
-,MoneyBalance INT
+        CustomerID INT
+        ,BadgeID INT
+        ,FullName NVARCHAR(64)
+        ,DesignationName NVARCHAR(64)
+        ,JoinedOn DATE
+        ,TerminatedOn DATE
+        ,AccountThead NVARCHAR(64)
+        ,MoneyBalance INT
 );
+
 INSERT INTO @TerminatedEmployee
 SELECT e.Id EmployeeID
-,Cast(dbo.tobdt(e.TerminatedOn) AS DATE) TerminatedOn
+        ,Cast(dbo.tobdt(e.TerminatedOn) AS DATE) TerminatedOn
+
 FROM Employee e
 WHERE e.TerminatedOn IS NOT NULL
 AND Cast(dbo.tobdt(e.TerminatedOn) AS DATE) >= @StartDate
 AND Cast(dbo.tobdt(e.TerminatedOn) AS DATE) < @EndDate
---select * from @TerminatedEmployee
+
 SELECT @RunTime = count(ID)
 FROM @TerminatedEmployee;
 WHILE @RunTime >= 1
 BEGIN
 INSERT INTO @TerminatedEmployeeMoneyBalance
-SELECT c.id CustomerID
-,e.BadgeId
-,e.FullName
-,da.DesignationName
-,Cast(dbo.tobdt(e.JoinedOn) AS DATE) JoinedOnOn
-,Cast(dbo.tobdt(e.TerminatedOn) AS DATE) TerminatedOn
-,ac.accounthead
-,SUM(t.Amount) Amount
+SELECT  c.id CustomerID
+        ,e.BadgeId
+        ,e.FullName
+        ,da.DesignationName
+        ,Cast(dbo.tobdt(e.JoinedOn) AS DATE) JoinedOnOn
+        ,Cast(dbo.tobdt(e.TerminatedOn) AS DATE) TerminatedOn
+        ,ac.accounthead
+        ,SUM(t.Amount) Amount
+
 FROM accounting.account ac
 JOIN accounting.txn t ON t.accountid = ac.id
 JOIN accounting.event ev ON ev.id = t.eventid
@@ -126,12 +130,12 @@ FROM @TerminatedEmployee
 WHERE ID = @RunTime
 )
 GROUP BY c.id
-,e.BadgeId
-,e.FullName
-,da.DesignationName
-,Cast(dbo.tobdt(e.JoinedOn) AS DATE)
-,Cast(dbo.tobdt(e.TerminatedOn) AS DATE)
-,ac.accounthead
+        ,e.BadgeId
+        ,e.FullName
+        ,da.DesignationName
+        ,Cast(dbo.tobdt(e.JoinedOn) AS DATE)
+        ,Cast(dbo.tobdt(e.TerminatedOn) AS DATE)
+        ,ac.accounthead
 SET @RunTime = @RunTime - 1;
 END
 SELECT *
